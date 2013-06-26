87e6d201e8952633962dcd77a545adffc4e73a31
Christian Foellmann <foellmann@foe-services.de>
2013-06-26 08:38:29 +0200

    protect file from direct access

--- html-mode-locker.php
+++ html-mode-locker.php
@@ -12,6 +12,13 @@ Text Domain: html-mode-locker
 Domain Path: /languages
 */
 
+//avoid direct calls to this file
+if ( ! function_exists( 'add_filter' ) ) {
+	header('Status: 403 Forbidden');
+	header('HTTP/1.1 403 Forbidden');
+	exit();
+}
+
 if ( !is_admin() )
   return;
 
